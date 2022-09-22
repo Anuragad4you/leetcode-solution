77d7990c041c6e28b6a5dbc856801e3c7f0dd7f1
Anuragad4you <55174071+Anuragad4you@users.noreply.github.com>
2022-09-23 00:58:52 +0530

    Time: 64 ms (11.77%), Space: 14.6 MB (13.29%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     string reverseWords(string s){
         
-        string str="",str2="";
+        
        int i=0,j=0;
+        string str1="",ans="";
        for(int k=0;k<s.length();k++){
-           
+           if(s[k]!=' '){
+                str1+=s[k];
+           }
+          
           if(s[k]==' '){
-              j=k;
-               reverse(s.begin()+i, s.begin()+j);
-              i=j+1;
               
-               
+              reverse(str1.begin(), str1.end());
+              
+              
+            ans+=str1+" ";  
+                str1="";
           } 
           // reverse(s.begin()+i,s.length()-1);
           
            
            
        }
-        reverse(s.begin()+i,s.end());
-        return s;
+        reverse(str1.begin(), str1.end());
+            ans=ans+str1; 
+        
+        return ans;
         
     }
 };
